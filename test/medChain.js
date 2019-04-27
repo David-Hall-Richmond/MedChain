ec8fdd48a6d3fcd881d5332d4238fbc77c4b00e4
rpate027 <rpate027@odu.edu>
2019-04-27 09:38:39 -0400

    Updated a typo and changed some output text

--- test/medChain.js
+++ test/medChain.js
@@ -18,16 +18,16 @@ contract("MedChain", function(accounts) {
         }).then(function(provider) {
             assert.equal(provider[0], 1, "contains the correct id");
             assert.equal(provider[1], "Provider 1", "contains the correct name");
-            assert.equal(provider[2], 0, "contains the correct authenticates count");
+            assert.equal(provider[2], 0, "contains the correct authentication count");
             return MedChainInstance.providers(2);
         }).then(function(provider) {
             assert.equal(provider[0], 2, "contains the correct id");
             assert.equal(provider[1], "Provider 2", "contains the correct name");
-            assert.equal(provider[2], 0, "contains the correct authenticates count");
+            assert.equal(provider[2], 0, "contains the correct authentication count");
         });
     });
 
-    it("allows a authenticator to cast a authenticate", function() {
+    it("allows an authenticator to authenticate", function() {
         return MedChain.deployed().then(function(instance) {
             MedChainInstance = instance;
             providerId = 1;
@@ -42,7 +42,7 @@ contract("MedChain", function(accounts) {
             return MedChainInstance.providers(providerId);
         }).then(function(provider) {
             var authenticateCount = provider[2];
-            assert.equal(authenticateCount, 1, "increments the provider's authenticate count");
+            assert.equal(authenticateCount, 1, "increments the provider's authentication count");
         })
     });
 
@@ -55,11 +55,11 @@ contract("MedChain", function(accounts) {
             return MedChainInstance.providers(1);
         }).then(function(provider1) {
             var authenticateCount = provider1[2];
-            assert.equal(authenticateCount, 1, "provider 1 did not receive any authenticates");
+            assert.equal(authenticateCount, 1, "Provider 1 did not receive any authentication");
             return MedChainInstance.providers(2);
         }).then(function(provider2) {
             var authenticateCount = provider2[2];
-            assert.equal(authenticateCount, 0, "provider 2 did not receive any authenticates");
+            assert.equal(authenticateCount, 0, "Provider 2 did not receive any authentication");
         });
     });
 });
