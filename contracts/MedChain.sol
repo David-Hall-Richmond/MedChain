9596299eb027308965c899f22de3c8d3f8a27d74
dhall <dhall006@odu.edu>
2019-04-28 19:40:43 -0400

    Completed demo implementation

--- contracts/MedChain.sol
+++ contracts/MedChain.sol
@@ -4,7 +4,7 @@ contract MedChain {
 
     //Addresses for sample patients
     //need to create accounts on MetaMask for these
-    address public address1=0xdCA5F28457838416A67FE0f0fabb7c1eF981926c ;
+    address public address1=0xdCA5F28457838416A67FE0f0fabb7c1eF981926c;
     address public address2=0xF00f3032d37b68857250Ad8faaF06bcD115e23a5;
 
     struct Patient {
@@ -17,22 +17,33 @@ contract MedChain {
     //Patient list
     mapping(address => Patient) public patients;
     uint public patientCount=0;
+    address[] public patientAddresses;
 
     constructor () public {
-        addPatient(address1,"Alice Adams","file1.json");
-        addPatient(address2,"Bob Barkins","file2.json");
+        addPatient(address1,"Alice Adams","patientAlice.json");
+        addPatient(address2,"Bob Barkins","patientBob.json");
     }
 
     function addPatient (address _address,string memory name,string memory recs) private {
         patients[_address] = Patient(_address,name,recs);
+        patientAddresses.push(_address);
         patientCount++;
     }
 
-    function addAuthorization (address patientAddress, string memory providerHash,uint providerID) public {
+    function addAuthorization (address patientAddress, string memory providerHash) public {
         patients[patientAddress].authList[providerHash] = true;
+    }
+
+    function removeAuthorization (address patientAddress, string memory providerHash) public {
+        patients[patientAddress].authList[providerHash] = false;
     }
 
     function checkAuth(address patientAddress, string memory providerHash) public returns(bool) {
         return patients[patientAddress].authList[providerHash];
+    }
+
+    function getPatientRecordFile(address patientAddress) public returns(string memory retString)    {
+        Patient memory ret = patients[patientAddress];
+        return ret.records;
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,28 @@
 App = {
+    /* Variables*/
     web3Provider: null,
     contracts: {},
     account: '0x0',
-    /** Does not need to be modified*/
+    currentDoctor: null,
+    patientRecords: $("#patientRecords"),
+
+provider1: {
+        practiceName: "Dr Alice",
+        practiceID: "314159",
+        nonce:0,
+        privateKey: ""
+    },
+    provider2: {
+        practiceName: "Dr Bob",
+        practiceID: "1235813",
+        nonce:0,
+        privateKey: ""
+    },
+
+    /* Functions to initialize the App
+    do not need to be modified*/
     init: function() {
+        console.log("calling initWeb3");
         return App.initWeb3();
     },
 
@@ -12,10 +31,12 @@ App = {
             // If a web3 instance is already provided by Meta Mask.
             App.web3Provider = web3.currentProvider;
             web3 = new Web3(web3.currentProvider);
+            console.log(web3.version.api);
         } else {
             // Specify default instance if no web3 instance provided
             App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
             web3 = new Web3(App.web3Provider);
+            console.log(web3.version.api);
         }
         return App.initContract();
     },
@@ -26,9 +47,14 @@ App = {
             App.contracts.MedChain = TruffleContract(meds);
             // Connect provider to interact with contract
             App.contracts.MedChain.setProvider(App.web3Provider);
-
             return App.render();
         });
+    },
+
+    initProviders: function() {
+        App.provider1.privateKey=web3.sha3(App.provider1.name);
+        App.provider2.privateKey=web3.sha3(App.provider1.name);
+        App.currentDoctor = App.provider1;
     },
 
     render: function() {
@@ -40,7 +66,7 @@ App = {
         web3.eth.getCoinbase(function(err, account) {
             if (err === null) {
                 App.account = account;
-                //$("#accountAddress").html("Your Account: " + account);
+                $("#accountAddress").html("Your Account: " + account);
             }
         });
         /** Modify after this only**/
@@ -48,87 +74,194 @@ App = {
         // Load contract data
         App.contracts.MedChain.deployed().then(function(instance) {
             medChainInstance = instance;
+            App.initProviders();
             return medChainInstance.patientCount();
         }).then(function(patientCount) {
+            //console.log(patientCount);
+            //console.log(patientCount.c[0]);
             var patientSelect = $('#patientSelect');
             patientSelect.empty();
 
-            // This needs to be fixed, below is a dummy function just to get things working
-            /*for (var i = 1; i <= patientCount; i++) {
+            for (var i = 0; i <= patientCount.c[0]; i++) {
                 medChainInstance.patientAddresses(i).then(function(patientAdd) {
-                    medChainInstance.patients[patientAdd].then(function(patient){
-                        var name = patient.name;
+                    //console.log("Adding patient address: "+patientAdd);
 
+                    medChainInstance.patients(patientAdd).then(function(patient){
+                        //console.log(patient);
+                        var name = patient[1];
+                        //console.log("Adding patient name: "+ name);
                         var patientOption = "<option value='" + patientAdd + "' >" + name + "</ option>";
                         patientSelect.append(patientOption);
                     });
                 });
-            }*/
-            for (var i = 1; i <= patientCount; i++) {
-                        var name = "Bob Jones";
-                        var patientAdd = "Dummy Address";
-
-                        var patientOption = "<option value='" + patientAdd + "' >" + name + "</ option>";
-                        patientSelect.append(patientOption);
-
-
             }
-
             loader.hide();
             content.show();
+            App.patientRecords.hide();
+            testDriver(medChainInstance);
         }).catch(function(error) {
             console.warn(error);
         });
     },
 
-    getRecords: function() {
+    loadGetRecords(){
+        App.patientRecords.hide();
+        $("#retrieveButton").show();
+        $("#addButton").hide();
+    },
+
+    loadAddRecords(){
+        App.patientRecords.hide();
+        $("#retrieveButton").hide();
+        $("#addButton").show();
+    },
+
+    /* Primary Application Behavior Functions*/
+    // Need to implement
+    getAuthorization: function(patientAddress,provider){
+        return new Promise(function (resolve,reject) {
+            App.contracts.MedChain.deployed().then(function(instance)
+            {
+               //console.log("Checking authorization");
+                var stringToHash = provider.nonce + provider.privateKey;
+                var hashedString = web3.sha3(stringToHash);
+                console.log("Checking authorization with hash: " + hashedString);
+                console.log("For patient: " + patientAddress);
+                instance.checkAuth.call(patientAddress, hashedString).then(function(result) {
+                    resolve(result);
+                });//.then(function(result){
+            });
+        });
+    },
+    getRecordsHandler(){
+        App.getRecords(App.currentDoctor);
+    },
+    newAuthHandler(){
+        App.patientRecords.hide();
+        App.createNewAuthorization(App.account,App.currentDoctor);
+    },
+    addRecordHandler(){
+        App.patientRecords.hide();
+        App.addRecord(App.currentDoctor);
+    },
+    getRecords: function(provider) {
+        var patientRecords = $("#patientRecords");
+        console.log("getRecords was called");
         var patientAddress = $('#patientSelect').val();
-        App.contracts.medChain.deployed().then(function(instance) {
-            console.log("Getting Authorization: ");
-            console.log(instance.checkAuth(patientAddress, "123456"));
-            return instance.auth(patientAddress, { from: App.account });
-        }).then(function(result) {
-            console.log(result);
+        App.contracts.MedChain.deployed().then(function(instance) {
+            App.getAuthorization(patientAddress,provider).then(function(result){
+                if(result){
+                    console.log("Getting patient record file");
+                    instance.getPatientRecordFile.call(patientAddress).then(function(result){
+                        console.log("got here");
+                        patientRecords.show();
+                        console.log(result);
+                        fetch(result)
+                            .then(response => response.json())
+                            .then(jsonResponse =>$("#patientRecords").html(JSON.stringify(jsonResponse,null,2)));
+                    }).catch(function(err){
+                        console.log(err);
+                    })
+                }
+                else{
+                    window.alert("You are not authorized to view this patient's records!");
+                }
+            });
+        }).catch(function(err) {
+            console.error(err);
+        });
+    },
+    addRecord: function(provider){
+        var patientAddress = $('#patientSelect').val();
+        App.contracts.MedChain.deployed().then(function(instance) {
+            App.getAuthorization(patientAddress,provider).then(function(result){
+                if(result){
+                    instance.getPatientRecordFile.call(patientAddress).then(function(result){
+                        console.log(result);
+                        var fn = result;
+                        fetch(fn)
+                            .then(response => response.json())
+                            .then(jsonResponse =>{
+                                var jsonData = jsonResponse;
+                                jsonData.push({
+                                    "name": "Alice",
+                                    "age": 25,
+                                    "height": 65,
+                                    "weight": 136,
+                                    "bloodPressure":{
+                                        "sys": 125,
+                                        "dia": 95
+                                    },
+                                    "scripts": ["EpiPen","Allergy Shots"],
+                                    "allergies": [
+                                        "tree pollen",
+                                        "Oak",
+                                        "bees"],
+                                    "conditions":[
+                                    ],
+                                    "doctorOfRecord": "Dr Bob",
+                                    "dateOfRecord":"12/13/2015"
+                                });
+                                console.log(JSON.stringify(jsonData,null,2));
+                                App.download(JSON.stringify(jsonData,null,2),fn,'text/plain');
+                            });
+                    }).catch(function(err){
+                        console.log(err);
+                    })
+                }
+                else{
+                    window.alert("You are not authorized to view this patient's records!");
+                }
+            });
         }).catch(function(err) {
             console.error(err);
         });
     },
 
-    // Need to implement
-    getAuthorization: function(){
-        // Create hash of private key and nonce
-        // Get patient address
-        // Call checkAuth() contract function
-        // Call checkAuth() contract function
-        // Return
+    download: function(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
     },
 
-    addRecord: function(){
-        // Call getAuthorization()
-        // Open connection to file
-        // Append new record to file
-        // Close file
-    },
+    createNewAuthorization(patientAdd,provider){
 
-    createNewAuthorization(){
-        // Get patient information from MetaMask
-        // Create hash
-        // Send info into addAuthorization() in Contract
-    },
-
-   /* listenForEvents: function() {
-        App.contracts.Election.deployed().then(function(instance) {
-            instance.votedEvent({}, {
-                fromBlock: 0,
-                toBlock: 'latest'
-            }).watch(function(error, event) {
-                console.log("event triggered", event)
-                // Reload when a new vote is recorded
-                App.render();
+        App.contracts.MedChain.deployed().then(function(instance) {
+            var stringToHash = provider.nonce + provider.privateKey;
+            var hashedString = web3.sha3(stringToHash);
+            console.log("Creating new authorization with hash" + hashedString);
+            console.log("For patient: " + patientAdd);
+            instance.addAuthorization(patientAdd, hashedString).then(function(){
+                location.reload(true);
             });
         });
-    }*/
+    },
+
+    removeAuthorization(patientAdd,provider){
+        App.contracts.MedChain.deployed().then(function(instance) {
+            var stringToHash = provider.nonce + provider.privateKey;
+            var hashedString = web3.sha3(stringToHash);
+            console.log("Removing authorization with hash" + hashedString);
+            console.log("For patient: " + patientAdd);
+            instance.removeAuthorization(patientAdd, hashedString);
+        });
+
+    }
+
 };
+
+async function testDriver(){
+    //App.removeAuthorization("0xdca5f28457838416a67fe0f0fabb7c1ef981926c",(App.currentDoctor));
+    //App.createNewAuthorization(App.account,App.currentDoctor);
+    console.log(await App.getAuthorization(App.account, App.currentDoctor));
+    //App.removeAuthorization(App.account,App.currentDoctor);
+    //console.log("Current address:" + App.account);
+    console.log(await App.getAuthorization("0xF00f3032d37b68857250Ad8faaF06bcD115e23a5", App.currentDoctor));
+}
+
+/* Utility Functions*/
 
 // No need to modify
 $(function() {
@@ -138,218 +271,3 @@ $(function() {
 });
 
 
-////////////////////////////////////////////////////////////////
-
-// Section for nonceHash is probaly ok, 
-// everything else may help some though its no good
-// best to get rid of rest from here down.
-
-/* Section for nonceHash in js
-
-        var Web3 = require("web3")
-        String message = "Private Key"
-        web3.eth.accounts.hashMessage(message)
-
-        String nonceKey = "12345";
-        // E.g. "00000" :
-        String zeroGoal = new String(new char[nonceKey.length()]).replace("\0", "0");
-
-        long nonce = 0;
-        boolean isNonceFound = false;
-        String nonceHash = "";
-
-        long start = Date.now();
-
-        while (!isNonceFound) {
-
-            nonceHash = web3.eth.accounts.hashMessage(message + nonce);
-            isNonceFound = nonceHash.substring(0, nonceKey.length()).equals(zeroGoal);
-            if (!isNonceFound) {
-                nonce++;
-            }
-        }
-
-        long ms = Date.now() - start;
-
-        console.log(String.format("Nonce: %d", nonce));
-        console.log(String.format("Nonce Hash: %s", nonceHash));
-        console.log(String.format("Nonce Search Time: %s ms", ms));
-*/
-
-/*
-        GitHost.prototype.hash = function () {
-  return this.committish ? '#' + this.committish : ''
-}
-        var patientHash = $('#patientSelect').val();
-
-        // Get patient address
-            // Call checkAuth() contract function
-            console.log(instance.checkAuth(patientHash, "1234567"));
-            // Call checkAuth() contract function
-            console.log(instance.checkAuth(patientAddress, "123456"));
-            // Return
-            return instance.auth(patientAddress, { from: App.account });
-        }).then(function(result) {
-            console.log(result);
-        }).catch(function(err) {
-            console.error(err);
-        });
-    },
-
-    addRecord: function(){
-        // Call getAuthorization()
-        getAuthorization()
-
-        // Open connection to file
-
-    var currentFilePath = opts.currentFilePath;
-
-    opts.logger.debug("Reading the file: %s", currentFilePath);
-
-    var read;
-
-    try {
-        read = fs.readFileSync(currentFilePath, "utf8");
-    } catch (e) {
-        opts.errored = true;
-        return opts.logger.info(messages.fileNotFound(path.basename(currentFilePath)));
-    }
-
-    var found = false;
-
- var fs$open = fs.open
-  fs.open = open
-  function open (path, flags, mode, cb) {
-    if (typeof mode === 'function')
-      cb = mode, mode = null
-
-    return go$open(path, flags, mode, cb)
-
-    function go$open (path, flags, mode, cb) {
-      return fs$open(path, flags, mode, function (err, fd) {
-        if (err && (err.code === 'EMFILE' || err.code === 'ENFILE'))
-          enqueue([go$open, [path, flags, mode, cb]])
-        else {
-          if (typeof cb === 'function')
-            cb.apply(this, arguments)
-          retry()
-        }
-      })
-    }
-  }
-
-        // Server Example
-        ```js
-const WebSocket = require('ws');
-
-const wss = new WebSocket.Server({ port: 8080 });
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-
-  ws.send('something');
-});
-```
-// Express js example
-```js
-const express = require('express');
-const http = require('http');
-const url = require('url');
-const WebSocket = require('ws');
-
-const app = express();
-
-app.use(function (req, res) {
-  res.send({ msg: "hello" });
-});
-
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', function connection(ws, req) {
-  const location = url.parse(req.url, true);
-  // You might use location.query.access_token to authenticate or share sessions
-  // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-
-  ws.send('something');
-});
-
-server.listen(8080, function listening() {
-  console.log('Listening on %d', server.address().port);
-});
-```
-        // Append new record to file
-
-        errorEx.append = function (str, def) {
-    return {
-        message: function (v, message) {
-            v = v || def;
-
-            if (v) {
-                message[0] += ' ' + str.replace('%s', v.toString());
-            }
-
-            return message;
-        }
-    };
-};
-utils.append = function(val) {
-  return function(node) {
-    append(this, val, node);
-  };
-};
-function append(compiler, val, node) {
-  if (typeof compiler.append !== 'function') {
-    return compiler.emit(val, node);
-  }
-  return compiler.append(val, node);
-}
-
-        // Close file
-
-        // Returns instance of FSWatcher for chaining.
-FSWatcher.prototype.close = function() {
-  if (this.closed) return this;
-
-  this.closed = true;
-  Object.keys(this._closers).forEach(function(watchPath) {
-    this._closers[watchPath]();
-    delete this._closers[watchPath];
-  }, this);
-  this._watched = Object.create(null);
-
-  this.removeAllListeners();
-  return this;
-};
-    },
-
-    createNewAuthorization(){
-        get patient information from MetaMask
-        // Create hash
-GitHost.prototype.hash = function () {
-  return this.committish ? '#' + this.committish : ''
-}
-        // Send info into addAuthorization() in Contract
-function send (req, path, options) {
-  return new SendStream(req, path, options)
-}
-
-  function Contract(contract) {
-    var self = this;
-    var constructor = this.constructor;
-    this.abi = constructor.abi;
-
-    if (typeof contract == "string") {
-      var address = contract;
-      var contract_class = constructor.web3.eth.contract(this.abi);
-      contract = contract_class.at(address);
-    }
-
-    },
-*/
